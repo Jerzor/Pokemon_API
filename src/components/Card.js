23beb198f383b1345502028afe67bb39af5ea1f0
Jerzor <jerzor2000@vp.pl>
2021-07-26 10:31:22 +0200

    visual fixes in card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,8 +4,22 @@ import "../styles/Card.css";
 const Card = ({ pokemon }) => {
   return (
     <div className="cardContainer">
-      <h3 className="cardName">{pokemon.name}</h3>
+      <div className="cardHeader">
+        <h3 className="cardName">{pokemon.name}</h3>
+        <p>No. {pokemon.number}</p>
+      </div>
       <img className="cardImg" src={pokemon.imageUrlHiRes} alt={pokemon.name} />
+      <div className="cardFooter">
+        <p>
+          <span>Rarity:</span> {pokemon.rarity ? pokemon.rarity : "???"}
+        </p>
+        <p>
+          <span>Series:</span> {pokemon.series ? pokemon.series : "???"}
+        </p>
+        <p>
+          <span>Set:</span> {pokemon.set ? pokemon.set : "???"}
+        </p>
+      </div>
     </div>
   );
 };
